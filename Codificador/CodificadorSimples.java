32e61d5d093bdf8358d47a76b189e8d6c39244bc
PedroGY01 <pgiovenardiyounes@gmail.com>
2025-08-04 21:48:26 +0000

    classePedro

--- Codificador/CodificadorSimples.java
+++ Codificador/CodificadorSimples.java
@@ -23,6 +23,21 @@ public class CodificadorSimples implements Codificador {
         return encoded.toString();
     }
 
+    public String codifica2(String str) {
+        char[] caracteres = str.toCharArray();
+        char[] invertido = new char[caracteres.length];
+
+        for (int i = 0; i < caracteres.length; i++) {
+            invertido[i] = caracteres[caracteres.length - 1 - i];
+        }
+
+        for (int i = 0; i < invertido.length; i++) {
+            invertido[i] = (char) (invertido[i] + 1);
+        }
+
+        return new String(invertido);
+    }
+
     public String decodifica(String str) {
         StringBuilder encoded = new StringBuilder();
         
@@ -31,5 +46,22 @@ public class CodificadorSimples implements Codificador {
         }
         
         return encoded.toString();
+    }
+
+     public String decodifica2(String str) {
+        char[] caracteres = str.toCharArray();
+
+        // Subtrai 1 do código ASCII de cada caractere
+        for (int i = 0; i < caracteres.length; i++) {
+            caracteres[i] = (char) (caracteres[i] - 1);
+        }
+
+        // Inverte a string
+        char[] invertido = new char[caracteres.length];
+        for (int i = 0; i < caracteres.length; i++) {
+            invertido[i] = caracteres[caracteres.length - 1 - i];
+        }
+
+        return new String(invertido);
     }
 }
