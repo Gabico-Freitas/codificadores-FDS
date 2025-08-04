881264becbf5044651399d752be5f4fa04b0ba4d
Gabico-Freitas <gabriel.f.silva278@gmail.com>
2025-08-04 18:53:07 -0300

    Update Factory.java

--- Codificador/Factory.java
+++ Codificador/Factory.java
@@ -1,7 +1,13 @@
 public class Factory {
     public static String bestEncoder(int securityLevel){
-        if (securityLevel==1) {
+         if (securityLevel==1) {
             return "CodificadorSimples \nCodificadorVerde";
+        }
+        else if(securityLevel==Integer.MAX_VALUE){
+            return "CodificadorBest";
+        }
+        else if(securityLevel==Integer.MIN_VALUE){
+            return "CodificadorWorst";
         }
         else{
             return "outro";
